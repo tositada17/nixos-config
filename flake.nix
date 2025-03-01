35581eb9c0a9ef61887f2c98185813401b2a5e31
tositada <tositada17@gmail.com>
2025-03-01 03:59:34 +0000

    first commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+  
+  outputs = { self, nixpkgs, ...}@inputs:{
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem{
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
