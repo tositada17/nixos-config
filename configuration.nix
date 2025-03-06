5e2c71b2265c51f7f022bd5236e6809522182fe3
tositada <tositada17@gmail.com>
2025-03-06 17:50:08 +0900

    change keyMap

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-    keyMap = "us";
+    keyMap = "jp106";
     # useXkbConfig = true; # use xkb.options in tty.
   };
 
