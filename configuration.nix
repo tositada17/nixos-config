9c67eb4602f174c6930e0d30e348e53a477b998d
tositada <tositada17@gmail.com>
2025-03-25 14:45:17 +0900

    remove wpa_supplicant

--- configuration.nix
+++ configuration.nix
@@ -26,7 +26,7 @@
   # Network Settings
   networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
