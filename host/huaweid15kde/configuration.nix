5a9cdbc65330688f5aee22a9be87acdcf1d0bcbc
tositada <tositada17@gmail.com>
2025-04-02 13:20:51 +0900

    add locale

--- host/huaweid15kde/configuration.nix
+++ host/huaweid15kde/configuration.nix
@@ -69,6 +69,30 @@
   # add /etc/x11/xkb
   services.xserver.exportConfiguration = true;
 
+  # locale
+  i18n = {
+    supportedLocales = [
+      "ja_JP.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+    ];
+    extraLocaleSettings = {
+      LANGUAGE = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      LC_CTYPE = "en_US.UTF8";
+      LC_ADDRESS = "en_US.UTF8";
+      LC_IDENTIFICATION = "en_US.UTF8";
+      LC_MEASUREMENT = "en_US.UTF8";
+      LC_MESSAGES = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF8";
+      LC_NAME = "en_US.UTF8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF8";
+      LC_TELEPHONE = "en_US.UTF8";
+      LC_TIME = "en_US.UTF8";
+      LC_COLLATE = "en_US.UTF8";
+    };
+  };
+
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
   # services.xserver.xkb.options = "eurosign:e,caps:escape";
